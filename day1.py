05d58a7ae3c6639917bfce63f1c625b340076481
hwarang <phrpp5770@gmail.com>
2023-01-12 17:51:12 +0900

    fifth

--- day1.py
+++ day1.py
@@ -1,18 +1,8 @@
-# 실험 3
+# 실험 4
 
-subjects = {
-    "의사소통영어": "A0",
-    "데이터베이스": "A+",
-    "회귀분석": "B+",
-    }
-student = '박화랑'
-subject = '데이터베이스'
-print(student, '학생의', subject, '성적은', subjects[subject])
+import tkinter as tk
 
-# f 스트링 기법
-print(f'{student}학생의 {subject}과목 성적은 {subjects[subject]}입니다')
-# modern style
-#print("{0}학생의 {1}과목 성적은 {2}입니다.". format(student, subject, subjects[subject]))
-
-# old style
-print("%s학생의 %s과목 성적은 %s입니다" % (student, subject, subjects[subject]))
+win = tk.Tk()
+win.geometry('400x300')
+win.title('파이썬 1일차 priview')
+win.mainloop()
