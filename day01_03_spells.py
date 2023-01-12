f7ef36a8b13f8cc94310f734e0da4e27b1ca9e81
hwarang <phrpp5770@gmail.com>
2023-01-12 16:27:19 +0900

    day01_01_temperature first

---
+++ day01_03_spells.py
@@ -0,0 +1,16 @@
+subjects = {
+    "의사소통영어": "A0",
+    "데이터베이스": "A+",
+    "회귀분석": "B+",
+    }
+student = '박화랑'
+subject = '데이터베이스'
+print(student, '학생의', subject, '성적은', subjects[subject])
+
+# f 스트링 기법
+print(f'{student}학생의 {subject}과목 성적은 {subjects[subject]}입니다')
+# modern style
+#print("{0}학생의 {1}과목 성적은 {2}입니다.". format(student, subject, subjects[subject]))
+
+# old style
+print("%s학생의 %s과목 성적은 %s입니다" % (student, subject, subjects[subject]))
